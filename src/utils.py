4b4ccfc29eb936b68b97cc27f169c70a96757679
George Perdrizet <george@perdrizet.org>
2024-11-22 16:55:08 -0500

    Implemented gridsearch hyperparameter tuning.

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,28 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
+'''Notebook helper functions for common operations.'''
+
 import pandas as pd
+from sklearn.model_selection import KFold, cross_val_score
 
-# load the .env file variables
-load_dotenv()
+def cross_val(model, features: pd.DataFrame, labels: pd.Series) -> list:
+    '''Reusable helper function to run cross-validation on a model. Takes model,
+    Pandas data frame of features and Pandas data series of labels. Returns 
+    list of cross-validation fold accuracy scores as percents.'''
 
+    # Define the cross-validation strategy
+    cross_validation=KFold(n_splits=7, shuffle=True, random_state=315)
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+    # Run the cross-validation, collecting the scores
+    scores=cross_val_score(
+        model,
+        features,
+        labels,
+        cv=cross_validation,
+        n_jobs=-1,
+        scoring='accuracy'
+    )
+
+    # Print mean and standard deviation of the scores
+    print(f'Cross-validation accuracy: {(scores.mean() * 100):.2f} +/- {(scores.std() * 100):.2f}%')
+
+    # Return the scores
+    return scores
